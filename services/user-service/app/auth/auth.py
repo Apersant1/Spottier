80f9d76887b74e2b7ddf397379e28e8676bd217e
Apersant1 <miracles.on.the.turn@gmail.com>
2023-09-18 01:10:24 +0300

    clear useless import

--- services/user-service/app/auth/auth.py
+++ services/user-service/app/auth/auth.py
@@ -1,16 +1,13 @@
 from config import SECRET
 import uuid
-from typing import Optional
 
-from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin
+from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import (
     AuthenticationBackend,
     CookieTransport,
     JWTStrategy,
 )
-from fastapi_users.db import SQLAlchemyUserDatabase
-from database.database import User, get_user_db
+from database.database import User
 from manager import get_user_manager
 
 
