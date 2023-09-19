75c5c87e668bc117a90787fc1b4dcb9c09495f52
Apersant1 <miracles.on.the.turn@gmail.com>
2023-09-19 18:22:39 +0300

    add DockerFile

--- services/user-service/app/config.py
+++ services/user-service/app/config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
+DB_PORT = 5432
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
