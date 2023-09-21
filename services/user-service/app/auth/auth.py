cd784fc2617838b68ba0cdebba9759d08b9cfaac
Apersant1 <miracles.on.the.turn@gmail.com>
2023-09-21 14:11:36 +0300

    refactor auth module

--- services/user-service/app/auth/auth.py
+++ services/user-service/app/auth/auth.py
@@ -1,28 +1,24 @@
-import uuid
-
-from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import (
     AuthenticationBackend,
     CookieTransport,
     JWTStrategy,
 )
-from ..database import User
-from ..manager import get_user_manager
 
-SECRET = 'SECRET'
-cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600)
+class AuthInitializer():
+    def __init__(self):
+        self.secret_phrase: str|None = None
+        self.cookie_transport = None
+        self.auth_backend = None
+    def initializer(self,secret):
+        self.secret_phrase = secret
+        self.cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600)
+        self.auth_backend = AuthenticationBackend(
+            name="jwt",
+            transport=self.cookie_transport,
+            get_strategy=self.get_jwt_strategy,
+        )
+
+    def get_jwt_strategy(self) -> JWTStrategy:
+        return JWTStrategy(secret=self.secret_phrase, lifetime_seconds=3600)
 
 
-def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
-
-
-auth_backend = AuthenticationBackend(
-    name="jwt",
-    transport=cookie_transport,
-    get_strategy=get_jwt_strategy,
-)
-
-fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
-
-current_active_user = fastapi_users.current_user(active=True)
