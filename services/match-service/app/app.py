1b28a0e73c5220f64ca88f54f4e10293c43600af
Apersant1 <miracles.on.the.turn@gmail.com>
2023-10-11 14:41:30 +0300

    Compiled in Docker and added a tag

--- services/match-service/app/app.py
+++ services/match-service/app/app.py
@@ -26,27 +26,27 @@ def get_db():
 
 # endpoints
 
-@app.post("/matches", status_code=201, response_model=MatchRead, summary="Add new match")
+@app.post("/matches", tags=["matches"],status_code=201, response_model=MatchRead, summary="Add new match")
 async def add_spot(match: MatchCreate, db: Session = Depends(get_db)) -> MatchCreate:
     return crud.create_match(match=match, db=db)
 
 
-@app.get("/matches", status_code=201, response_model=list[MatchRead], summary="get all matches")
+@app.get("/matches", tags=["matches"],status_code=201, response_model=list[MatchRead], summary="get all matches")
 async def get_all_matches(page: int = Query(1, ge=1), limit: int = Query(10, ge=1, le=100), db: Session = Depends(get_db)) -> list[MatchRead]:
     return db.query(Match).offset((page - 1) * limit).limit(limit).all()
 
 
-@app.get("/matches/{matchId}", status_code=201, response_model=MatchRead, summary="Get match by id")
+@app.get("/matches/{matchId}", tags=["matches"],status_code=201, response_model=MatchRead, summary="Get match by id")
 async def get_match(matchId: uuid.UUID, db: Session = Depends(get_db)) -> MatchRead:
     return crud.get_match(matchId=matchId, db=db)
 
 
-@app.patch("/matches/{matchId}", status_code=201, response_model=MatchRead, summary="Update match by id")
+@app.patch("/matches/{matchId}",tags=["matches"], status_code=201, response_model=MatchRead, summary="Update match by id")
 async def update_match(matchId: uuid.UUID, match: MatchUpdate, db: Session = Depends(get_db)) -> MatchUpdate:
     return crud.update_match_by_id(matchId=matchId, match=match, db=db)
 
 
-@app.delete("/matches/{matchId}", status_code=201, response_model=MatchRead, summary="remove match by id")
+@app.delete("/matches/{matchId}", tags=["matches"],status_code=201, response_model=MatchRead, summary="remove match by id")
 async def delete_match(matchId: uuid.UUID, db: Session = Depends(get_db)) -> MatchRead:
     if crud.delete_match(matchId, db):
         return JSONResponse(status_code=200, content={"message": "Item successfully deleted"})
