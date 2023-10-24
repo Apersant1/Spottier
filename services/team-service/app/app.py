6ca3e31386f982ea90ed8f195da58fcef66f1ded
Apersant1 <miracles.on.the.turn@gmail.com>
2023-10-24 14:22:33 +0300

    refactor team-service

--- services/team-service/app/app.py
+++ services/team-service/app/app.py
@@ -49,36 +49,62 @@ Instrumentator().instrument(app).expose(app)
 @app.post('/teams',tags=["teams"],status_code=status.HTTP_201_CREATED,response_model=TeamRead)
 def create_team(team:TeamCreate,db:Session = Depends(get_db)) -> TeamCreate:
     return crud.create_team(team=team,db=db)
+   
 
 
 @app.get("/teams",tags=["teams"],status_code=status.HTTP_200_OK,response_model=list[TeamRead])
 async def get_all_teams(db:Session = Depends(get_db)):
-    return crud.get_all_teams(db=db)
+    all_teams = crud.get_all_teams(db=db)
+    if all_teams is not None:
+        return all_teams
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
+            content={"Error":"Teams not found"})
 
 @app.get("/teams/{teamId}",tags=["teams"],status_code=status.HTTP_200_OK,response_model=TeamRead)
 async def get_team_by_id(teamId:uuid.UUID,db:Session = Depends(get_db)):
-    return crud.get_team(teamId=teamId,db=db)
+    db_team = crud.get_team(teamId=teamId,db=db)
+    if db_team is not None:
+        return db_team
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
+        content={"Error":"Team not found"})
 
 
 @app.patch("/teams/{teamId}",tags=["teams"],status_code=status.HTTP_200_OK,response_model=TeamRead)
 async def update_team_by_id(teamId:uuid.UUID,team:TeamUpdate,db:Session = Depends(get_db)):
-    return crud.update_team(teamId=teamId,team=team,db=db)
-    
+    updated_team = crud.update_team(teamId=teamId,team=team,db=db)
+    if updated_team != None:
+        return updated_team
+    return JSONResponse(status_code=404, content={"message": "Team not found"})
 
 
 @app.delete("/teams/{teamId}",tags=["teams"],)
 async def delete_team_by_id(teamId:uuid.UUID,db:Session = Depends(get_db)):
-    return crud.delete_team(teamId=teamId,db=db)
+    deleted_team = crud.delete_team(teamId=teamId,db=db)
+    if deleted_team:
+        return JSONResponse(status_code=status.HTTP_200_OK,\
+        content={"Message":"Team deleted successfully"})
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
+        content={"Error":"Team not found"})
 
 
 @app.patch("/teams/{teamId}/add-users/{userId}",tags=["teams"],response_model=TeamRead)
 async def add_user_in_team(teamId:uuid.UUID,userId:uuid.UUID,db:Session = Depends(get_db)):
-    return crud.add_user_team(teamId=teamId,userId=userId,db=db)
-  
+    add_user_team = crud.add_user_team(teamId=teamId,userId=userId,db=db)
+    if add_user_team is not None:
+        return add_user_team
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
+        content={"Error":"Team not found"})
      
     
 
-@app.patch("/teams/{teamId}/delete-users/{userId}",tags=["teams"])
+@app.patch("/teams/{teamId}/delete-users/{userId}",tags=["teams"],response_model=TeamRead)
 async def delete_user_from_team(teamId:uuid.UUID,userId:uuid.UUID,db:Session = Depends(get_db)):
-    return crud.delete_user_team(teamId=teamId,userId=userId,db=db)
-
+    remove_user_from_team = crud.delete_user_team(teamId=teamId,userId=userId,db=db)
+    if remove_user_from_team == 1:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
+        content={"Error":"Team not found"})
+    elif remove_user_from_team == 2:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
+        content={"Error":"User is not in team"})
+    else:
+        return remove_user_from_team
--- services/team-service/app/crud.py
+++ services/team-service/app/crud.py
@@ -1,31 +1,12 @@
 import uuid
 from datetime import datetime
-from fastapi import status
-from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from .models import Team
 from .schemas import TeamCreate,TeamUpdate
 
-def get_all_teams(db:Session):
-    all_teams = db.query(Team).all()
-    if all_teams is not None:
-        return all_teams
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-            content={"Error":"Teams not found"})
-    
-
-def get_team(teamId:uuid.UUID,db:Session):
-    db_team = db.query(Team).filter(Team.id == teamId).first()
-    if db_team is not None:
-        return db_team
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-        content={"Error":"Team not found"})
-
-
 def create_team(team:TeamCreate,db:Session) -> TeamCreate:
-    try:
-        db_team = Team(
+    db_team = Team(
         id = uuid.uuid4(),
         name = team.name,
         desc = team.desc,
@@ -33,81 +14,64 @@ def create_team(team:TeamCreate,db:Session) -> TeamCreate:
         members=[],
         created_at = datetime.utcnow()
         )
-        db.add(db_team)
-        db.commit()
-        db.refresh(db_team)
-        return db_team
-    except SQLAlchemyError as e:
-        db.rollback()
-        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN,content={"Error":e})
+    db.add(db_team)
+    db.commit()
+    db.refresh(db_team)
+    return db_team
+    
+
+def get_all_teams(db:Session):
+    return db.query(Team).all()
+    
+    
+
+def get_team(teamId:uuid.UUID,db:Session):
+    return db.query(Team).filter(Team.id == teamId).first()
+    
+
+
 
 def update_team(teamId:uuid.UUID,team:TeamUpdate,db:Session):
     '''
     Update info about team
     '''
-    result = db.query(Team)\
+    result =  db.query(Team)\
         .filter(Team.id == teamId)\
         .update(team.dict())
     db.commit()
 
     if result == 1:
         return get_team(teamId=teamId, db=db)
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-        content={"Error":"Team not found"})
+    return None
 
 def delete_team(teamId:uuid.UUID,db:Session):
-    result = db.query(Team).filter(Team.id == teamId).first()
-    if result is not None:
-        result = db.query(Team).filter(Team.id == teamId).delete()
-        db.commit()
-        return JSONResponse(status_code=status.HTTP_200_OK,\
-        content={"Message":"Team deleted successfully"})
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-        content={"Error":"Team not found"})
+    result = db.query(Team).filter(Team.id == teamId).delete()
+    db.commit()
+    return result == 1
 
 def add_user_team(teamId:uuid.UUID,userId:uuid.UUID,db:Session):
-    try:
-        result = get_team(teamId=teamId,db=db)
-        
-        if isinstance(result,JSONResponse):
-            return result
-    
-        if userId in result.members:
-            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,content={"Error":"This user is already a member of this team"})
-        else:
-            db.query(Team)\
+    add_user_team = db.query(Team)\
                 .filter(Team.id == teamId) \
                 .update({
                 Team.members: Team.members + [userId],
                 Team.member_count: Team.member_count + 1
                 })
-        db.commit()
-        return get_team(teamId=teamId, db=db)
-    except SQLAlchemyError as e:
-        db.rollback()
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    db.commit()
+    return get_team(teamId=teamId, db=db)
 
 def delete_user_team(teamId:uuid.UUID,userId:uuid.UUID,db:Session):
-    try:
-        result = get_team(teamId=teamId,db=db)
-        if isinstance(result,JSONResponse):
-            return result
-
-        if userId in result.members:
-            result.members.remove(userId)
-        else:
-            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-                content={"Error":"This user is not on this team"})
-
-        db.query(Team) \
-            .filter(Team.id == teamId) \
-            .update({
-                Team.members: result.members,
-                Team.member_count: Team.member_count - 1
-            })
-        db.commit()
-        return JSONResponse(status_code=status.HTTP_200_OK,\
-            content={"Success":"The user has been removed from the team"})
-    except SQLAlchemyError as e:
-        db.rollback()
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,content={"Error":e})
+    result = get_team(teamId=teamId,db=db)
+    if result is None:
+        return 1
+    if userId in result.members:
+        result.members.remove(userId)
+    else:
+        return 2
+    db.query(Team) \
+        .filter(Team.id == teamId) \
+        .update({
+            Team.members: result.members,
+            Team.member_count: Team.member_count - 1
+        })
+    db.commit()
+    return get_team(teamId=teamId,db=db)
