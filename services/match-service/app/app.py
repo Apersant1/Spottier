7a2b489017423982d4284e5e7ededd5ef0acbb73
Apersant1 <miracles.on.the.turn@gmail.com>
2025-01-03 15:07:49 +0300

    fix: update libs

--- services/match-service/app/app.py
+++ services/match-service/app/app.py
@@ -1,13 +1,12 @@
 import uuid
 import logging
 import logging_loki
-from fastapi import FastAPI, Depends, Query,status
+from fastapi import FastAPI, Depends, Query, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from prometheus_fastapi_instrumentator import Instrumentator
-from .schemas import Match, MatchCreate, MatchRead, MatchUpdate
-from .models import Match
+from .schemas import MatchCreate, MatchRead, MatchUpdate
 from .database import DB_INITIALIZER
 from . import crud
 from . import config
@@ -28,19 +27,20 @@ logger.addHandler(MatchHandler)
 # connect to DB
 SessionLocal = DB_INITIALIZER.init_database(str(cfg.postgres_dsn))
 logger.info(
-    "Create session", 
+    "Create session",
     extra={"tags": {"service": "Match-service"}},
 )
 app = FastAPI(title="Match-service")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Allows all origins
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"], # Allows all methods
-    allow_headers=["*"], # Allows all headers
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 Instrumentator().instrument(app).expose(app)
+
 
 def get_db():
     db = SessionLocal()
@@ -52,39 +52,88 @@ def get_db():
 
 # endpoints
 
-@app.post("/matches", tags=["matches"],status_code=201, response_model=MatchRead, summary="Add new match")
-async def add_spot(match: MatchCreate, db: Session = Depends(get_db)) -> MatchCreate:
+
+@app.post(
+    "/matches",
+    tags=["matches"],
+    status_code=201,
+    response_model=MatchRead,
+    summary="Add new match",
+)
+async def add_spot(
+    match: MatchCreate, db: Session = Depends(get_db)
+) -> MatchCreate:
     return crud.create_match(match=match, db=db)
 
 
-@app.get("/matches", tags=["matches"],status_code=201, response_model=list[MatchRead], summary="get all matches")
-async def get_all_matches(page: int = Query(1, ge=1), limit: int = Query(10, ge=1, le=100), db: Session = Depends(get_db)) -> list[MatchRead]:
-    all_matches = crud.get_all_match(page=page,limit=limit,db=db)
+@app.get(
+    "/matches",
+    tags=["matches"],
+    status_code=201,
+    response_model=list[MatchRead],
+    summary="get all matches",
+)
+async def get_all_matches(
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1, le=100),
+    db: Session = Depends(get_db),
+) -> list[MatchRead]:
+    all_matches = crud.get_all_match(page=page, limit=limit, db=db)
     if all_matches is not None:
         return all_matches
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-            content={"Error":"Matchs not found"})
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"Error": "Matchs not found"},
+    )
 
-@app.get("/matches/{matchId}", tags=["matches"],status_code=200, response_model=MatchRead, summary="Get match by id")
-async def get_match(matchId: uuid.UUID, db: Session = Depends(get_db)) -> MatchRead:
+
+@app.get(
+    "/matches/{matchId}",
+    tags=["matches"],
+    status_code=200,
+    response_model=MatchRead,
+    summary="Get match by id",
+)
+async def get_match(
+    matchId: uuid.UUID, db: Session = Depends(get_db)
+) -> MatchRead:
     db_match = crud.get_match(matchId=matchId, db=db)
     if db_match is not None:
         return db_match
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-        content={"Error":"Match not found"})
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"Error": "Match not found"},
+    )
 
 
-@app.patch("/matches/{matchId}",tags=["matches"], status_code=200, response_model=MatchRead, summary="Update match by id")
-async def update_match(matchId: uuid.UUID, match: MatchUpdate, db: Session = Depends(get_db)) -> MatchUpdate:
+@app.patch(
+    "/matches/{matchId}",
+    tags=["matches"],
+    status_code=200,
+    response_model=MatchRead,
+    summary="Update match by id",
+)
+async def update_match(
+    matchId: uuid.UUID, match: MatchUpdate, db: Session = Depends(get_db)
+) -> MatchUpdate:
     updated_match = crud.update_match_by_id(matchId=matchId, match=match, db=db)
     if updated_match is not None:
         return updated_match
     return JSONResponse(status_code=404, content={"message": "Match not found"})
 
-@app.delete("/matches/{matchId}", tags=["matches"],status_code=201, response_model=MatchRead, summary="remove match by id")
-async def delete_match(matchId: uuid.UUID, db: Session = Depends(get_db)) -> MatchRead:
+
+@app.delete(
+    "/matches/{matchId}",
+    tags=["matches"],
+    status_code=201,
+    response_model=MatchRead,
+    summary="remove match by id",
+)
+async def delete_match(
+    matchId: uuid.UUID, db: Session = Depends(get_db)
+) -> MatchRead:
     if crud.delete_match(matchId, db):
-        return JSONResponse(status_code=200, content={"message": "Match successfully deleted"})
+        return JSONResponse(
+            status_code=200, content={"message": "Match successfully deleted"}
+        )
     return JSONResponse(status_code=404, content={"message": "Match not found"})
-
-
--- services/team-service/app/app.py
+++ services/team-service/app/app.py
@@ -1,18 +1,17 @@
 import uuid
 import logging
 import logging_loki
-from fastapi import FastAPI,Depends,status
+from fastapi import FastAPI, Depends, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from prometheus_fastapi_instrumentator import Instrumentator
-from prometheus_client import start_http_server
-from .schemas import TeamRead,TeamCreate,TeamUpdate
+from .schemas import TeamRead, TeamCreate, TeamUpdate
 from .database import DB_INITIALIZER
 from . import config
 from . import crud
 
-cfg : config.Config = config.load_config()
+cfg: config.Config = config.load_config()
 
 TeamHandler = logging_loki.LokiHandler(
     url=cfg.loki_dsn,
@@ -28,13 +27,14 @@ logger.addHandler(TeamHandler)
 SessionLocal = DB_INITIALIZER.init_database(str(cfg.postgres_dsn))
 
 logger.info(
-    "Create session", 
+    "Create session",
     extra={"tags": {"service": "Team-service"}},
 )
 
+
 def get_db():
     """
-        Get DB session
+    Get DB session
     """
     db = SessionLocal()
     try:
@@ -43,76 +43,129 @@ def get_db():
         db.close()
 
 
-app = FastAPI(title="Team-service",version="0.1.0")
+app = FastAPI(title="Team-service", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Allows all origins
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"], # Allows all methods
-    allow_headers=["*"], # Allows all headers
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 Instrumentator().instrument(app).expose(app)
 
-@app.post('/teams',tags=["teams"],status_code=status.HTTP_201_CREATED,response_model=TeamRead)
-def create_team(team:TeamCreate,db:Session = Depends(get_db)) -> TeamCreate:
-    return crud.create_team(team=team,db=db)
-   
+
+@app.post(
+    "/teams",
+    tags=["teams"],
+    status_code=status.HTTP_201_CREATED,
+    response_model=TeamRead,
+)
+def create_team(team: TeamCreate, db: Session = Depends(get_db)) -> TeamCreate:
+    return crud.create_team(team=team, db=db)
 
 
-@app.get("/teams",tags=["teams"],status_code=status.HTTP_200_OK,response_model=list[TeamRead])
-async def get_all_teams(db:Session = Depends(get_db)):
+@app.get(
+    "/teams",
+    tags=["teams"],
+    status_code=status.HTTP_200_OK,
+    response_model=list[TeamRead],
+)
+async def get_all_teams(db: Session = Depends(get_db)):
     all_teams = crud.get_all_teams(db=db)
     if all_teams is not None:
         return all_teams
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-            content={"Error":"Teams not found"})
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"Error": "Teams not found"},
+    )
 
-@app.get("/teams/{teamId}",tags=["teams"],status_code=status.HTTP_200_OK,response_model=TeamRead)
-async def get_team_by_id(teamId:uuid.UUID,db:Session = Depends(get_db)):
-    db_team = crud.get_team(teamId=teamId,db=db)
+
+@app.get(
+    "/teams/{teamId}",
+    tags=["teams"],
+    status_code=status.HTTP_200_OK,
+    response_model=TeamRead,
+)
+async def get_team_by_id(teamId: uuid.UUID, db: Session = Depends(get_db)):
+    db_team = crud.get_team(teamId=teamId, db=db)
     if db_team is not None:
         return db_team
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-        content={"Error":"Team not found"})
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"Error": "Team not found"},
+    )
 
 
-@app.patch("/teams/{teamId}",tags=["teams"],status_code=status.HTTP_200_OK,response_model=TeamRead)
-async def update_team_by_id(teamId:uuid.UUID,team:TeamUpdate,db:Session = Depends(get_db)):
-    updated_team = crud.update_team(teamId=teamId,team=team,db=db)
+@app.patch(
+    "/teams/{teamId}",
+    tags=["teams"],
+    status_code=status.HTTP_200_OK,
+    response_model=TeamRead,
+)
+async def update_team_by_id(
+    teamId: uuid.UUID, team: TeamUpdate, db: Session = Depends(get_db)
+):
+    updated_team = crud.update_team(teamId=teamId, team=team, db=db)
     if updated_team is not None:
         return updated_team
     return JSONResponse(status_code=404, content={"message": "Team not found"})
 
 
-@app.delete("/teams/{teamId}",tags=["teams"],)
-async def delete_team_by_id(teamId:uuid.UUID,db:Session = Depends(get_db)):
-    deleted_team = crud.delete_team(teamId=teamId,db=db)
+@app.delete(
+    "/teams/{teamId}",
+    tags=["teams"],
+)
+async def delete_team_by_id(teamId: uuid.UUID, db: Session = Depends(get_db)):
+    deleted_team = crud.delete_team(teamId=teamId, db=db)
     if deleted_team:
-        return JSONResponse(status_code=status.HTTP_200_OK,\
-        content={"Message":"Team deleted successfully"})
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-        content={"Error":"Team not found"})
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"Message": "Team deleted successfully"},
+        )
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"Error": "Team not found"},
+    )
 
 
-@app.patch("/teams/{teamId}/add-users/{userId}",tags=["teams"],response_model=TeamRead)
-async def add_user_in_team(teamId:uuid.UUID,userId:uuid.UUID,db:Session = Depends(get_db)):
-    add_user_team = crud.add_user_team(teamId=teamId,userId=userId,db=db)
+@app.patch(
+    "/teams/{teamId}/add-users/{userId}",
+    tags=["teams"],
+    response_model=TeamRead,
+)
+async def add_user_in_team(
+    teamId: uuid.UUID, userId: uuid.UUID, db: Session = Depends(get_db)
+):
+    add_user_team = crud.add_user_team(teamId=teamId, userId=userId, db=db)
     if add_user_team is not None:
         return add_user_team
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-        content={"Error":"Team not found"})
-     
-    
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"Error": "Team not found"},
+    )
 
-@app.patch("/teams/{teamId}/delete-users/{userId}",tags=["teams"],response_model=TeamRead)
-async def delete_user_from_team(teamId:uuid.UUID,userId:uuid.UUID,db:Session = Depends(get_db)):
-    remove_user_from_team = crud.delete_user_team(teamId=teamId,userId=userId,db=db)
+
+@app.patch(
+    "/teams/{teamId}/delete-users/{userId}",
+    tags=["teams"],
+    response_model=TeamRead,
+)
+async def delete_user_from_team(
+    teamId: uuid.UUID, userId: uuid.UUID, db: Session = Depends(get_db)
+):
+    remove_user_from_team = crud.delete_user_team(
+        teamId=teamId, userId=userId, db=db
+    )
     if remove_user_from_team == 1:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-        content={"Error":"Team not found"})
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"Error": "Team not found"},
+        )
     elif remove_user_from_team == 2:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,\
-        content={"Error":"User is not in team"})
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"Error": "User is not in team"},
+        )
     else:
         return remove_user_from_team
